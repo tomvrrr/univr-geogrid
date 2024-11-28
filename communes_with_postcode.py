47ba5a95bb43e993c9d2ea50ee98f080aea15d3c
tomvrrr <thomas.verrier@sanofi.com>
2024-11-28 11:35:48 +0100

    Added postal code enrichment

---
+++ communes_with_postcode.py
@@ -0,0 +1,21 @@
+from communes_core import *
+
+## Loading Postal Code
+postcode = (
+    pd
+    .read_csv('https://datanova.laposte.fr/data-fair/api/v1/datasets/laposte-hexasmal/metadata-attachments/base-officielle-codes-postaux.csv',
+              dtype=str
+    )
+    .drop('ligne_5',axis = 1)
+    .drop_duplicates()
+    .rename(columns={
+        'code_commune_insee':   'COM',
+        '_geopoint':            'GPS_COORDINATES_2024',
+        'code_postal':          'CODE_POSTAL_2024'
+    })
+    [['COM','CODE_POSTAL_2024','GPS_COORDINATES_2024']]
+)
+
+## Enriching df
+df = df.merge(postcode,on = 'COM',how = 'left')
+df.loc[lambda d: d.CODE_POSTAL_2024.isna()|d.COM.duplicated(False)].to_excel('TEST.xlsx')
