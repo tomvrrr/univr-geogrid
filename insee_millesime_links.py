1b4ad410d0abac484276bf39b40865bde1fd5a85
tomvrrr <thomas.verrier@sanofi.com>
2024-11-28 11:36:18 +0100

    Collected all links for insee millesime

---
+++ insee_millesime_links.py
@@ -0,0 +1,39 @@
+COMMUNES = {
+    '2024':     'https://www.insee.fr/fr/statistiques/fichier/7766585/v_commune_2024.csv',
+    '2023':     'https://www.insee.fr/fr/statistiques/fichier/6800675/v_commune_2023.csv',
+    '2022':     'https://www.insee.fr/fr/statistiques/fichier/6051727/commune_2022.csv',
+    '2021':     'https://www.insee.fr/fr/statistiques/fichier/5057840/commune2021-csv.zip',
+    '2020':     'https://www.insee.fr/fr/statistiques/fichier/4316069/communes2020-csv.zip'
+}
+
+CANTONS = {
+    '2024':     'https://www.insee.fr/fr/statistiques/fichier/7766585/v_canton_2024.csv',
+    '2023':     'https://www.insee.fr/fr/statistiques/fichier/6800675/v_canton_2023.csv',
+    '2022':     'https://www.insee.fr/fr/statistiques/fichier/6051727/canton_2022.csv',
+    '2021':     'https://www.insee.fr/fr/statistiques/fichier/5057840/canton2021-csv.zip',
+    '2020':     'https://www.insee.fr/fr/statistiques/fichier/4316069/canton2020-csv.zip'
+}
+
+ARRONDISSEMENTS = {
+    '2024':     'https://www.insee.fr/fr/statistiques/fichier/7766585/v_arrondissement_2024.csv',
+    '2023':     'https://www.insee.fr/fr/statistiques/fichier/6800675/v_arrondissement_2023.csv',
+    '2022':     'https://www.insee.fr/fr/statistiques/fichier/6051727/arrondissement_2022.csv',
+    '2021':     'https://www.insee.fr/fr/statistiques/fichier/5057840/arrondissement2021-csv.zip',
+    '2020':     'https://www.insee.fr/fr/statistiques/fichier/4316069/arrondissement2020-csv.zip'
+}
+
+DEPARTMENT = {
+    '2024':     'https://www.insee.fr/fr/statistiques/fichier/7766585/v_departement_2024.csv',
+    '2023':     'https://www.insee.fr/fr/statistiques/fichier/6800675/v_departement_2023.csv',
+    '2022':     'https://www.insee.fr/fr/statistiques/fichier/6051727/departement_2022.csv',
+    '2021':     'https://www.insee.fr/fr/statistiques/fichier/5057840/departement2021-csv.zip',
+    '2020':     'https://www.insee.fr/fr/statistiques/fichier/4316069/departement2020-csv.zip'
+}
+
+REGION = {
+    '2024':     'https://www.insee.fr/fr/statistiques/fichier/7766585/v_region_2024.csv',
+    '2023':     'https://www.insee.fr/fr/statistiques/fichier/6800675/v_region_2023.csv',
+    '2022':     'https://www.insee.fr/fr/statistiques/fichier/6051727/region_2022.csv',
+    '2021':     'https://www.insee.fr/fr/statistiques/fichier/5057840/region2021-csv.zip',
+    '2020':     'https://www.insee.fr/fr/statistiques/fichier/4316069/region2020-csv.zip'
+}
